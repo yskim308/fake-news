13428ea4e2fbbfe07176ae83a9d158635d45c73b
Young Seo Kim <youngseokim7@gmail.com>
2025-10-08 15:38:19 +0900

    implement template generating function

---
+++ view/generate-page.go
@@ -0,0 +1,26 @@
+package view
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
+
+	"github.com/yskim308/fake-news/repository"
+)
+
+func GeneratePage(id int, repo *repository.Repository) string {
+	const templateFilePath = "./form.html"
+	tmpl := template.Must(template.ParseFiles(templateFilePath))
+
+	post, err := repo.GetEntry(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buffer bytes.Buffer
+	err = tmpl.Execute(&buffer, post)
+	generatedHTML := buffer.String()
+
+	return generatedHTML
+}
