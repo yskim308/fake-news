bef3167a06acc6c5f8f55f59889f7ca90d4eb524
Young Seo Kim <youngseokim7@gmail.com>
2025-10-10 21:19:45 +0900

    create the two main endpoints

--- main.go
+++ main.go
@@ -2,18 +2,54 @@ package main
 
 import (
 	"fmt"
-	"github.com/yskim308/fake-news/repository"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/yskim308/fake-news/repository"
+	"github.com/yskim308/fake-news/view"
 )
 
 func main() {
-	repo := repository.Repository{}
+	repo := &repository.Repository{}
 	repo.Connect()
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
+	})
+
+	http.HandleFunc("/news/articles/", func(w http.ResponseWriter, req *http.Request) {
+		path := req.URL.Path
+		pathPrefix := "/news/articles/"
+		if path == pathPrefix {
+			http.Error(w, "article ID missing", http.StatusBadRequest)
+		}
+
+		id, err := strconv.Atoi(strings.TrimPrefix(path, pathPrefix))
+		if err != nil {
+			http.Error(w, "id is not an integer, invalid path", http.StatusBadRequest)
+		}
+
+		generatedHTML := view.GeneratePage(id, repo)
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(generatedHTML))
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		tmpl, err := template.ParseFiles("./view/form.html")
+		if err != nil {
+			log.Fatal("template not found: ", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			log.Fatal("error executing template for form: ", err)
+		}
 	})
 
 	port := 4000
